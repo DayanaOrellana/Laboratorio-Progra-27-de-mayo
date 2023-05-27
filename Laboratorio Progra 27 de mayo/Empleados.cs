bb7ce2f924d7c442feca7073fc6b6d87a4df7af2
Dayana Escobar <dorellanae2@miumg.edu.gt>
2023-05-27 12:55:09 -0600

    Agregar archivos de proyecto.

---
+++ Laboratorio Progra 27 de mayo/Empleados.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_Progra_27_de_mayo
+{
+    internal class Empleados
+    {
+        public class Empleado
+        {
+            public int Id { get; set; }
+            public string Nombre { get; set; }
+            public string Apellido { get; set; }
+
+            public int Edad { get; set; }
+            public string Cargo { get; set; }
+
+            public int Sueldo { get; set; }
+
+            public Empleado(int id, string nombre, string apellido, int edad, string cargo)
+            {
+                Id = id;
+                Nombre = nombre;
+                this.Apellido = apellido;
+                Edad = edad;
+                Cargo = cargo;
+            }
+            public override string ToString()
+            {
+                return $"Id: {Id} Nombre: {Nombre} Apellido: {Apellido} Edad: {Edad} Cargo: {Cargo}";
+            }
+        }
+    }
+}
---
+++ Laboratorio Progra 27 de mayo/Program.cs
@@ -0,0 +1,51 @@
+﻿using Laboratorio_Progra_27_de_mayo;
+GestorEmpleados empleado1 = new GestorEmpleados();
+
+empleado1.Nombre = "Eduardo";
+empleado1.Apellido = "Rodriguez";
+empleado1.Edad = 21;
+empleado1.Cargo = "Jefe";
+empleado1.Sueldo = 5000;
+
+
+
+GestorEmpleados empleado2 = new GestorEmpleados();
+
+empleado2.Nombre = "Jose";
+empleado2.Apellido = "Sandoval";
+empleado2.Edad = 21;
+empleado2.Cargo = "Gerente";
+empleado2.Sueldo = 4000;
+
+GestorEmpleados empleado3= new GestorEmpleados();
+
+empleado3.Nombre = "Sara";
+empleado3.Apellido = "lopez";
+empleado3.Edad = 23;
+empleado3.Cargo = "Empleado";
+empleado3.Sueldo = 3000;
+
+GestorEmpleados empleado4 = new GestorEmpleados();
+
+empleado4.Nombre = "Sucelly";
+empleado4.Apellido = "Escobar";
+empleado4.Edad = 21;
+empleado4.Cargo = "Empleado";
+empleado4.Sueldo = 3000;
+
+GestorEmpleados empleado5 = new GestorEmpleados();
+
+empleado5.Nombre = "Fatima";
+empleado5.Apellido = "Contreras";
+empleado5.Edad = 21;
+empleado5.Cargo = "Empleado";
+empleado5.Sueldo = 3000;
+
+Console.WriteLine("El empleado", empleado1.Nombre, empleado1.Apellido,"de", empleado1.Edad,"con el cargo de",empleado1.Cargo,"su sueldo es de",empleado1.Sueldo);
+Console.WriteLine("El empleado", empleado2.Nombre, empleado2.Apellido, "de", empleado2.Edad, "con el cargo de", empleado2.Cargo, "su sueldo es de", empleado2.Sueldo);
+Console.WriteLine("El empleado", empleado3.Nombre, empleado3.Apellido, "de", empleado3.Edad, "con el cargo de", empleado3.Cargo, "su sueldo es de", empleado3.Sueldo);
+Console.WriteLine("El empleado", empleado4.Nombre, empleado4.Apellido, "de", empleado4.Edad, "con el cargo de", empleado4.Cargo, "su sueldo es de", empleado4.Sueldo);
+Console.WriteLine("El empleado", empleado5.Nombre, empleado5.Apellido, "de", empleado5.Edad, "con el cargo de", empleado5.Cargo, "su sueldo es de", empleado5.Sueldo);
+
+
+
